61e6ae32f0f21b7a7d5f7042b3a2d9106650b914
Marc Nitzsche <marc.nitzsche@live.de>
2017-03-06 15:05:02 +0100

    Add method to make EditBox read key-value pairs

--- app/components/EditBox/EditBox.js
+++ app/components/EditBox/EditBox.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import styles from './EditBox.css';
 
 import EditField from './EditField';
 
-const EditBox = ({ fields, id }) => (
-  <div className={styles.boxWrapper}>
-    <h3 className={styles.boxTitle}>Act #{id}</h3>
-    <div>
-      {
-        fields.map(field => (
-          <EditField
-            key={field.label}
-            label={field.label}
-            value={field.value}
-            onChange={console.log}
-          />
-        ))
-      }
+const propTypes = {
+  fields: PropTypes.object.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+const EditBox = ({ fields, title }) => {
+  // Transforms { myKey: 'myValue' } to [{ label: 'myKey', value: 'myValue'}]
+  const transformedFields = Object.keys(fields).map(
+    fieldName => ({ label: fieldName, value: fields[fieldName] })
+  );
+
+  return (
+    <div className={styles.boxWrapper}>
+      <h3 className={styles.boxTitle}>{title}</h3>
+      <div>
+        {
+          transformedFields.map(field => (
+            <EditField
+              key={field.label}
+              label={field.label}
+              value={field.value}
+              onChange={console.log}
+            />
+          ))
+        }
+      </div>
     </div>
-  </div>
-);
+  );
+};
+
+EditBox.propTypes = propTypes;
 
 export default EditBox;
--- app/components/EditScreen/EditScreen.js
+++ app/components/EditScreen/EditScreen.js
@@ -1,20 +1,61 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import styles from './EditScreen.css';
 
-import EditBox from '../EditBox/EditBox';
+import GeneralBox from '../EditBox/GeneralBox';
+import ActBox from '../EditBox/ActBox';
 
+const propTypes = {
+  sheet: PropTypes.shape({
+    details: PropTypes.shape({
+      general: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        date: PropTypes.string.isRequired,
+        location: PropTypes.string.isRequired,
+        client: PropTypes.string.isRequired,
+        contact: PropTypes.string.isRequired,
+      }).isRequired,
+      schedule: PropTypes.shape({
+        start: PropTypes.string.isRequired,
+        rehearsal: PropTypes.string,
+        construction: PropTypes.string,
+        deconstruction: PropTypes.string,
+      }).isRequired,
+    }).isRequired,
+    actions: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        fields: PropTypes.shape(
+          PropTypes.shape({
+            label: PropTypes.string.isRequired,
+            value: PropTypes.string,
+          }).isRequired,
+        ).isRequired,
+      }).isRequired,
+    ).isRequired,
+  }).isRequired,
+};
 
 const EditScreen = ({ sheet: { details, actions } }) => (
   <div className={styles.editScreen}>
-    <h2 className={styles.title}>{details.title}</h2>
+    <h2 className={styles.title}>{details.general.title}</h2>
     <div className={styles.editContainer}>
+      <GeneralBox
+        fields={details.general}
+      />
+
       {
         actions.map(action => (
-          <EditBox fields={action.fields} key={action.id} id={action.id} />
+          <ActBox
+            fields={action.fields}
+            key={action.id}
+            id={action.id}
+          />
         ))
       }
     </div>
   </div>
 );
+
+EditScreen.propTypes = propTypes;
 
 export default EditScreen;
