ead9545e2987cda4ab465f30a9ce4dd04a5366f1
Marc Nitzsche <marc.nitzsche@live.de>
2017-03-06 14:57:28 +0100

    Make fields in object use key-value rather than extra object

--- app/actions/actionCreators/index.js
+++ app/actions/actionCreators/index.js
@@ -16,11 +16,13 @@ export function setExampleSheet() {
 
 const exampleSheet = {
   details: {
-    title: 'Minifestival',
-    date: '22.02.2017',
-    location: 'Aula des Marie-Curie-Gymnasiums',
-    client: 'Abijahrgang 2017',
-    contact: 'Marc Nitzsche, Flo Geißler',
+    general: {
+      title: 'Minifestival',
+      date: '10.10.2017',
+      location: 'Aula des Marie-Curie-Gymnasiums',
+      client: 'Abijahrgang 2017',
+      contact: 'Marc Nitzsche, Flo Geißler',
+    },
     schedule: {
       start: '22.02.2017 18:00 Uhr',
       rehearsal: '22.02.2017 12:30 Uhr',
@@ -54,79 +56,34 @@ const exampleSheet = {
   },
   actions: [
     {
-      id: '1',
-      fields: [
-        {
-          label: 'title',
-          value: 'Gravity',
-        },
-        {
-          label: 'description',
-          value: 'Song von Sara Bareilles',
-        },
-        {
-          label: 'length',
-          value: '5 Minuten',
-        },
-        {
-          label: 'artist',
-          value: 'Rebecca Schönknecht',
-        },
-        {
-          label: 'requirements',
-          value: '1 HH, Piano',
-        },
-      ],
+      id: 1,
+      fields: {
+        title: 'Gravity',
+        description: 'Song von Sara Bareilles',
+        length: '5 Minutes',
+        artist: 'Rebecca Schönknecht',
+        requirements: '1 HH, Piano',
+      },
     },
     {
-      id: '2',
-      fields: [
-        {
-          label: 'title',
-          value: 'Imagine',
-        },
-        {
-          label: 'description',
-          value: 'Song von John Lennon',
-        },
-        {
-          label: 'length',
-          value: '3 Minuten',
-        },
-        {
-          label: 'artist',
-          value: 'Flo Geißler, Vera Gennrich',
-        },
-        {
-          label: 'requirements',
-          value: '2 HH, DI-Box',
-        },
-      ],
+      id: 2,
+      fields: {
+        title: 'Imagine',
+        description: 'Song von John Lennon',
+        length: '3 Minutes',
+        artist: 'Flo Geißler, Vera Gennrich',
+        requirements: '2 HH, DI-Box',
+      },
     },
     {
-      id: '3',
-      fields: [
-        {
-          label: 'title',
-          value: 'Probleme',
-        },
-        {
-          label: 'description',
-          value: 'Das ist echte Lyrik.',
-        },
-        {
-          label: 'length',
-          value: '4 Minuten',
-        },
-        {
-          label: 'artist',
-          value: 'Lucas Schemenz',
-        },
-        {
-          label: 'requirements',
-          value: '1 HH, Mikrofonständer',
-        },
-      ],
+      id: 3,
+      fields: {
+        title: 'Probleme',
+        description: 'Das ist echte Lyrik.',
+        length: '4 Minutes',
+        artist: 'Lucas Schemenz',
+        requirements: '1 HH, Mikrofonständer',
+      },
     }
   ],
 };
