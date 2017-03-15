535ed27b6d84ef22e6cf18ecacef35d41716426f
Marc Nitzsche <marc.nitzsche@live.de>
2017-03-15 20:56:58 +0100

    Add PrintScreen component

---
+++ app/components/PrintScreen/PrintScreen.js
@@ -0,0 +1,83 @@
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+import { ipcRenderer } from 'electron';
+
+import Logo from '../Logo/Logo';
+import PrintCard from './PrintCard/PrintCard';
+import PrintLayout from './PrintLayout/PrintLayout';
+
+import './print.global.css';
+
+
+const propTypes = {
+  general: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    client: PropTypes.string.isRequired,
+    contact: PropTypes.string.isRequired,
+  }).isRequired,
+  schedule: PropTypes.shape({
+    start: PropTypes.string.isRequired,
+    rehearsal: PropTypes.string,
+    construction: PropTypes.string,
+    deconstruction: PropTypes.string,
+  }).isRequired,
+  actions: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      fields: PropTypes.shape(
+        PropTypes.shape({
+          label: PropTypes.string.isRequired,
+          value: PropTypes.string,
+        }).isRequired,
+      ).isRequired,
+    }).isRequired,
+  ).isRequired,
+};
+
+const mapStateToProps = state => ({
+  general: state.general,
+  schedule: state.schedule,
+  requirements: state.requirements,
+  additionalDetails: state.additionalDetails,
+  actions: state.actions,
+});
+
+const PrintScreen = ({
+  general,
+  schedule,
+  actions }) => (
+    <PrintLayout
+      headerLeft={general.date}
+      headerMiddle={<Logo isTiny style={{ fontSize: 12 }} />}
+      footer={general.title}
+      pageNumberCount={Math.ceil(actions.length / 4)}
+    >
+      <div>
+        {
+          actions.map((action) => (
+            <PrintCard
+              key={action.id}
+              id={action.id}
+              title={action.fields.title}
+              description={action.fields.description}
+              length={action.fields.length}
+              artist={action.fields.artist}
+              requirements={action.fields.requirements}
+            />
+          ))
+        }
+      </div>
+    </PrintLayout>
+);
+
+PrintScreen.propTypes = propTypes;
+
+window.printPdf = () => ipcRenderer.send('create-and-open-pdf');
+
+export default connect(
+  mapStateToProps,
+  null,
+)(PrintScreen);
--- app/containers/App.js
+++ app/containers/App.js
@@ -8,39 +8,59 @@ import DragBar from '../components/DragBar/DragBar';
 
 import StartScreen from '../components/StartScreen/StartScreen';
 import EditScreen from '../components/EditScreen/EditScreen';
+import PrintScreen from '../components/PrintScreen/PrintScreen';
 
 import transitions from './AppTransitions.css';
 
 const propTypes = {
   editMode: PropTypes.bool.isRequired,
+  printMode: PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = state => ({
   editMode: state.editMode,
+  printMode: state.printMode,
 });
 
-const App = ({ editMode }) => (
-  <div>
-    <DragBar />
-    <HeaderLogo shouldShow={editMode} />
-    <ReactCSSTransitionGroup
-      transitionAppear
-      transitionName={transitions}
-      transitionAppearTimeout={800}
-      transitionEnterTimeout={500}
-      transitionLeaveTimeout={300}
-    >
-      {editMode
-      ? <div key="editScreen" style={{ width: '100%', height: '100%' }}>
-        <EditScreen />
-      </div>
-      : <div key="startScreen" style={{ width: '100%', height: '100%' }}>
+const App = ({ editMode, printMode }) => {
+  const Body = () => {
+    if (editMode) {
+      return (
+        <div key="editScreen" style={{ width: '100%', height: '100%' }}>
+          <EditScreen />
+        </div>
+      );
+    } else if (printMode) {
+      return (
+        <div key="printScreen" style={{ width: '100%', height: '100%' }}>
+          <PrintScreen />
+        </div>
+      );
+    }
+
+    return (
+      <div key="startScreen" style={{ width: '100%', height: '100%' }}>
         <StartScreen />
       </div>
-    }
-    </ReactCSSTransitionGroup>
-  </div>
-);
+    );
+  };
+
+  return (
+    <div>
+      <DragBar />
+      <HeaderLogo shouldShow={editMode} />
+      <ReactCSSTransitionGroup
+        transitionAppear
+        transitionName={transitions}
+        transitionAppearTimeout={800}
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={300}
+      >
+        {Body()}
+      </ReactCSSTransitionGroup>
+    </div>
+  );
+};
 
 App.propTypes = propTypes;
 
