5f61caea2cd1df254ef19dc45aaae44a9a221bd0
Marc Nitzsche <marc.nitzsche@live.de>
2017-03-15 20:41:59 +0100

    Add style prop to Logo

--- app/components/Logo/Logo.js
+++ app/components/Logo/Logo.js
@@ -3,14 +3,20 @@ import styles from './Logo.css';
 
 const propTypes = {
   isTiny: PropTypes.bool.isRequired,
+  style: PropTypes.object,
 };
 
-const Logo = ({ isTiny }) => (
-  <h1 className={isTiny ? styles.headerLogo : styles.bigLogo}>
+const defaultProps = {
+  style: {},
+};
+
+const Logo = ({ isTiny, style }) => (
+  <h1 className={isTiny ? styles.headerLogo : styles.bigLogo} style={style}>
     dispos.io
   </h1>
 );
 
 Logo.propTypes = propTypes;
+Logo.defaultProps = defaultProps;
 
 export default Logo;
